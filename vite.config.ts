20d6f14d0615da42413ab5be00844a4b02431781
manvote <manovate15@gmail.com>
2026-01-31 10:00:51 +0530

    Changes updated

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    middlewareMode: true,
+  },
+  preview: {
+    middlewareMode: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
